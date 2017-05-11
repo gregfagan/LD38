11e6b19af21ec855999146fb9361a878e9bfbb03
jkyle <jkyle@causethought.com>
2017-05-10 21:55:05 -0500

    Removing unused files.

--- src/actions/help.js
+++
@@ -1,3 +0,0 @@
-import { addToBuffer } from '@util'
-
-export default gameState => addToBuffer('Commands: GO <location>, TAKE <item>, LOOK (at <item>), INVENTORY, INPUT <value>, USE <item>, EXAMINE <item in inventory>')(gameState)
